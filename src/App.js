5e39011d1294e18945edcfe0f6bad5ee8c640006
AmitS9728 <amitsacco77@gmail.com>
2023-06-23 16:22:53 +0200

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState} from 'react';
+import Calendar from 'react-calendar';
 import './App.css';
+import Time from './Time.js'
 
 function App() {
+ const [date, setDate] = useState(new Date())
+ const [showTime, setShowTime] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Appointment calendar</h1>
+      <div className="calendar-container">
+        <Calendar onChange={setDate} value={date} onClickDay={() => setShowTime(true)}/>
+      </div>
+      {date.length > 0 ? (
+   <p>
+     <span>Start:</span>
+     {date[0].toDateString()}
+     &nbsp; 
+     &nbsp;
+     <span>End:</span>{date[1].toDateString()}
+   </p>
+        ) : (
+   <p>
+     <span>Default selected date:</span>{date.toDateString()}
+   </p>
+        )}
+     <Time showTime={showTime} date={date}/>   
     </div>
   );
 }
